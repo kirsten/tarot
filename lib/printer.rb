107b6d44228e1acd9d49d3f66ebed0d6388786c4
Kirsten Nordine <kirstennordine@gmail.com>
2015-08-17 00:59:26 -0400

    Printer prints actual cards

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,7 +1,48 @@
 class Printer
+  attr_reader :card_count
+
+  def initialize(options = {})
+    @card_count = options.fetch(:card_count) { 3 }
+  end
 
   def print(cards)
-    cards.each { |card| puts card.name }
+    horizontal_borders
+    12.times { vertical_borders }
+    row_with_card_names(cards)
+    12.times { vertical_borders }
+    horizontal_borders
+  end
+
+  def row_with_card_names(cards)
+    row = []
+    cards.each do |card|
+      borders = "||"
+      inner_width = card_width - borders.length
+      centered_card_name = card.name.center(inner_width, " ")
+      card_inner = borders.insert(1, centered_card_name)
+      row << card_inner
+    end
+    puts row.join('  ')
+  end
+
+  def horizontal_borders
+    borders = []
+    border = '-' * card_width
+    card_count.times { borders << border }
+    puts borders.join('  ')
+  end
+
+  def vertical_borders
+    borders = []
+    border = "||"
+    inner_width = card_width - border.length
+    border = border.insert(1, (" " * inner_width))
+    card_count.times { borders << border }
+    puts borders.join('  ')
+  end
+
+  def card_width
+    50
   end
 
 end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -12,11 +12,26 @@ describe Printer do
 
   subject(:printer) { Printer.new }
 
+  describe '#initialize' do
+    context 'when no card count is provided' do
+      it 'provides a default card count of 3' do
+        expect(printer.card_count).to eq 3
+      end
+    end
+
+    context 'when a card count is provided' do
+      it 'accepts the card count' do
+        printer = Printer.new({ card_count: 5 })
+        expect(printer.card_count).to eq 5
+      end
+    end
+  end
+
   describe '#print' do
-    it 'prints the names of each card' do
-      expect(STDOUT).to receive(:puts).with('The Tower')
-      expect(STDOUT).to receive(:puts).with('Three of Cups')
-      expect(STDOUT).to receive(:puts).with('The Hermit')
+    it 'prints each card' do
+      expect(printer).to receive(:horizontal_borders).twice
+      expect(printer).to receive(:vertical_borders).exactly(24).times
+      expect(printer).to receive(:row_with_card_names).once
       printer.print(cards)
     end
   end
